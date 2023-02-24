a7ef769552d4a615fe32eb76ef567eb11db592d4
hcampos <hcampospro@gmail.com>
2023-02-24 16:29:33 +0100

    💄 Add a main container for padding

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 //Other
 import React from 'react'
-import styled from 'styled-components'
 import { Routes, Route } from 'react-router-dom'
+import styled from 'styled-components'
 
 //Pages
 import Home from './pages/Home'
@@ -11,13 +11,21 @@ const App = () => {
   return (
     <div className='App'>
       <phone-mockup model='' color='black'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/search-pokemon/{id}' element={<Search />} />
-        </Routes>
+        <MainContainer>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/search-pokemon/:name' element={<Search />} />
+          </Routes>
+        </MainContainer>
       </phone-mockup>
     </div>
   )
 }
+
+const MainContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: #222831;
+`
 
 export default App
