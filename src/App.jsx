bf4a13e548107d81dd8948f367c4d945e960730c
hcampos <hcampospro@gmail.com>
2023-02-27 16:03:32 +0100

    💄 Add credits on the bottom of the page

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Search from './pages/Search'
 
 const App = () => {
   return (
-    <div className='App'>
+    <AppContainer>
       <phone-mockup model='' color='black'>
         <MainContainer>
           <Routes>
@@ -18,14 +18,38 @@ const App = () => {
           </Routes>
         </MainContainer>
       </phone-mockup>
-    </div>
+      <Credits>
+        Pokedex - @Saku - 4ème Concours du serveur discord -{' '}
+        <a
+          style={{ color: '#c8d6e5', textDecoration: 'underline' }}
+          href='https://discord.com/channels/655077317911117860/1041772720066674760/1077150811542532116'
+        >
+          Le Repaire du Web
+        </a>
+      </Credits>
+    </AppContainer>
   )
 }
+
+const AppContainer = styled.div`
+  position: relative;
+`
 
 const MainContainer = styled.div`
   height: 100%;
   width: 100%;
   background-color: #222831;
+`
+
+const Credits = styled.p`
+  position: fixed;
+  bottom: 0%;
+  left: 50%;
+  transform: translateX(-50%);
+  padding: 1em;
+  font-family: 'Tilt Neon', cursive;
+  color: #c8d6e5;
+  font-size: 1rem;
 `
 
 export default App
